99e763014faa0bf366f9b53cc9585a16504306d7
PavelPL9 <odinrg999@gmail.com>
2026-01-23 02:24:56 +0300

    Initial commit

--- src/theme_11/Lesson111.java
+++ src/theme_11/Lesson111.java
@@ -4,7 +4,6 @@ public class Lesson111 {
 
     public int findNumbers(int[] nums) {
         int numberOfEvenNumbers = 0;
-
         for (int num : nums) {
             int countOfDigits = 0;
             while (num != 0) {
