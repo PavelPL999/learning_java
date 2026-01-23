e9453d6b45889a054fd171f769d370e84156c2b0
PavelPL9 <odinrg999@gmail.com>
2026-01-23 03:21:37 +0300

    Initial commit

--- src/theme_11/Lesson113Palindrome.java
+++ src/theme_11/Lesson113Palindrome.java
@@ -4,7 +4,6 @@ public class Lesson113Palindrome {
 
     public boolean isPalindrome(int number) {
         boolean result = false;
-
         if (number < 0) {
             return result;
         }
@@ -17,7 +16,6 @@ public class Lesson113Palindrome {
             temp = temp / 10;
             reversed = reversed * 10 + lastDigit;
         }
-
         return number == reversed;
     }
 }
